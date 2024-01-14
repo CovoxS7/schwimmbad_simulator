fc434bbeb6eb67c4a06f6f28147158e2441d6c0a
CovoxS7 <suma.s7@gmx.de>
2024-01-14 11:14:42 +0100

    feat: adds functions for arrival

--- schwimmbad_simulator.c
+++ schwimmbad_simulator.c
@@ -16,15 +16,26 @@
 #define MAX_AUSLASTUNG		350
 #define MAX_BUS				50
 #define MAX_AUTO			5
+#define MAX_PARKPLATZ		30
 #define MAX_FUSSGAENGER		55
 
 /* Globale Variablen */
+int badegaesteGesamtMenge = 0;
+int badegaesteAktuelleMenge = 0;
 int busFahrgaeste = 0;
 int busHaltestelle = 0;
 int autoBelegung = 0;
+int autoParkplatz = 0;
+int fussgaengerJa = 0;
+int fussgaengerGesamt = 0;
 
 /* Funktionsdeklarationen */
 void simulation();
+void anreise(int simMinute);
+void busAnreise(int simMinute);
+void autoAnreise(int simMinute);
+void zuFussAnreise(int simMinute);
+void badegastHinzufuegen(int simMinute);
 int zufallszahl(int maximum);
 void eingabeVerarbeitung(int simMinute, int *simGeschwindigkeit);
 void ausgabeVerarbeitung(int simMinute);
@@ -36,33 +47,160 @@ int main(void) {
 	return 0;
 }
 
-/* Funktion verwaltet die Simulation */
+/* Funktion verwaltet den zeitlichen Ablauf der Simulation */
 void simulation() {
-	int simZeit = (11 * 60) + 1; /* (10h Öffnungszeit + 1h Vorlauf) * 60min + 1min um auf 20:00 zu kommen */
+	int simZeit = (11 * 60) + 1;	/* (10h Öffnungszeit + 1h Vorlauf) * 60min + 1min um auf 20:00 zu kommen */
 	int simGeschwindigkeit = 1000;
 	int simMinute; 
 	int simSekunde;
 	
-	/* Schleifeninhalt wird jede simulierte Minute aufgerufen (entsprechen 1 Realsekunde) */
+	/* Schleifeninhalt wird jede simulierte Minute aufgerufen (entsprechend 1 Realsekunde) */
 	/* i = 50 lässt die Simulation 09:50 starten für den ersten Bus */
 	for(simMinute = 50; simMinute < simZeit; simMinute++) {
 		
-		/* Schleifeninhalt wird jede simulierte Sekunde aufgerufen (entsprechen 1/60 Realsekunden) */
-		for(simSekunde = 0; simSekunde < 60; simSekunde++) {
-		
-		/* Hier werden die Funktionen für die einzelnen Abläufe aufgerufen */
+		/* Ruft die anreise-Funktion auf */
+		anreise(simMinute);
 			
-		}
+		/* Funktionen ab Öffnung des Schwimmbads */
+		if(simMinute > 59) {
+			/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+			/* Hier werden die Funktionen aufgerufen die jede Minute ausgeführt werden */
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			/* Fügt einen angereisten Badegast der Liste der Badegäste hinzu */
+			badegastHinzufuegen(simMinute);
+			
+			/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+			
+			/* Schleifeninhalt wird jede simulierte Sekunde aufgerufen (entsprechen 1/60 Realsekunden) */
+			for(simSekunde = 0; simSekunde < 60; simSekunde++) {
+				/* ------------------------------------------------------------------------ */
+				/* Hier werden die Funktionen aufgerufen die jede Sekunde ausgeführt werden */
+			
 		
+			
+			
+			
+			
+			
+			
+			
+				/* ------------------------------------------------------------------------ */
+			}
+		}
+
 		/* Eingabeverarbeitung */
 		eingabeVerarbeitung(simMinute, &simGeschwindigkeit);
 		
 		/* Anzeigen der Ergebnisse */
 		ausgabeVerarbeitung(simMinute);
 		
-		/* Lässt die Simulation nur alle 1 Sekunde durchlaufen */
+		/* Steuert die Simulationsgeschwindigkeit */
 		Sleep(simGeschwindigkeit);
+	}
+}
+
+/* Funktion Simuliert die Anreise der Badegäste */
+void anreise(int simMinute) {
+	int wahrscheinlichkeit;
+	
+	/* Alle 30 Minuten bringt der Bus neue Badegäste */
+	/* Erster Bus 9:50 Uhr, letzter Bus 19:10 Uhr */
+	if((simMinute - 20) % 30 == 0 && simMinute < 611) {
+		/* Der Bus bringt nur soviele Gäste, dass die maximale */
+		/* Auslastung des Schwimmbads nicht überschritten werden kann */
+		if(badegaesteAktuelleMenge <= (MAX_AUSLASTUNG - MAX_BUS - MAX_AUTO - 1)) {
+			busAnreise(simMinute);
+		}
+	}
+
+	/* Anreise ab Öffnung des Schwimmbads */
+	if(simMinute > 59) {
 		
+		/* Es besteht eine Wahrscheinlichkeit von 1:5 das in dieser Minute ein Auto ankommt */
+		wahrscheinlichkeit = zufallszahl(6);
+		if(wahrscheinlichkeit == 0 && autoParkplatz < MAX_PARKPLATZ) {
+			/* Mit dem Auto kommen nur soviele Gäste, dass die maximale */
+			/* Auslastung des Schwimmbads nicht überschritten werden kann */
+			if(badegaesteAktuelleMenge <= (MAX_AUSLASTUNG - MAX_AUTO - 1)) {
+				autoAnreise(simMinute);
+			}
+		}
+		
+		/* Es besteht eine Wahrscheinlichkeit von 1:10 das in dieser Minute ein Gast zu Fuss ankommt */
+		wahrscheinlichkeit = zufallszahl(11);
+		if(wahrscheinlichkeit == 0 && fussgaengerGesamt < MAX_FUSSGAENGER) {
+			/* Zu Fuß kommen nur soviele Gäste, dass die maximale */
+			/* Auslastung des Schwimmbads nicht überschritten werden kann */
+			if(badegaesteAktuelleMenge <= (MAX_AUSLASTUNG - 1)) {
+				zuFussAnreise(simMinute);
+			}
+		}
+	}
+}
+
+/* Funktion zur Generieren der Anzahl der Fahrgäste im Bus */
+void busAnreise(int simMinute) {
+	/* Je nach Uhrzeit bringt der Bus mehr oder weniger Gäste */
+	/* Maximal 50 in den Zeiten der Hauptauslastung des Schwimmbads */
+	/* In der letzten Stunde kommt kein Gast mehr zum Schwimmbad */
+	if(simMinute < 60) busFahrgaeste = zufallszahl(20);
+	if(simMinute > 60 && simMinute < 120) busFahrgaeste = zufallszahl(20) + 10;
+	if(simMinute > 120 && simMinute < 180) busFahrgaeste = zufallszahl(20) + 20;
+	if(simMinute > 180 && simMinute < 420) busFahrgaeste = zufallszahl(20) + 30;
+	if(simMinute > 420 && simMinute < 480) busFahrgaeste = zufallszahl(20) + 20;
+	if(simMinute > 480 && simMinute < 540) busFahrgaeste = zufallszahl(20) + 10;
+	if(simMinute > 540 && simMinute < 600) busFahrgaeste = zufallszahl(20);
+}
+
+/* Funktion zur Generierung der Anzahl der im Auto anreisenden Badegäste */
+void autoAnreise(int simMinute){
+	/* Je nach Uhrzeit kommen mehr oder weniger Gäste mit dem Auto */
+	/* Maximal 5 in den Zeiten der Hauptauslastung des Schwimmbads */
+	/* In der letzten Stunde kommt kein Gast mehr zum Schwimmbad */
+	if(simMinute < 60) autoBelegung = zufallszahl(2);
+	if(simMinute > 60 && simMinute < 120) autoBelegung = zufallszahl(2) + 1;
+	if(simMinute > 120 && simMinute < 180) autoBelegung = zufallszahl(2) + 2;
+	if(simMinute > 180 && simMinute < 420) autoBelegung = zufallszahl(2) + 3;
+	if(simMinute > 420 && simMinute < 480) autoBelegung = zufallszahl(2) + 2;
+	if(simMinute > 480 && simMinute < 540) autoBelegung = zufallszahl(2) + 1;
+	if(simMinute > 540 && simMinute < 600) autoBelegung = zufallszahl(2);
+	
+	/* Kommen Badegäste mit dem Auto wird ein Parkplatz belegt */
+	if(autoBelegung > 0) {
+		autoParkplatz++;
+	}
+}
+
+/* Funktion der zu Fuß anreisenden Badegäste */
+void zuFussAnreise(int simMinute){
+	/* In der letzten Stunde kommt kein Gast mehr zum Schwimmbad */
+	if(simMinute < 600) {
+		fussgaengerJa++;
+		fussgaengerGesamt++;
+	}
+}
+
+/* Funktion fügt die angereisten Personen den Badegästen hinzu */
+void badegastHinzufuegen(int simMinute) {
+	for(; busFahrgaeste > 0; busFahrgaeste--) {
+		badegaesteGesamtMenge++;
+		badegaesteAktuelleMenge++;
+	}	
+	for(; autoBelegung > 0; autoBelegung--) {
+		badegaesteGesamtMenge++;
+		badegaesteAktuelleMenge++;
+	}
+	for(; fussgaengerJa > 0; fussgaengerJa--) {
+		badegaesteGesamtMenge++;
+		badegaesteAktuelleMenge++;
 	}
 }
 
@@ -141,12 +279,12 @@ void ausgabeVerarbeitung(int simMinute) {
 	printf("\n\nBistro: %2d/20", 0);
 	printf("\nLiegen: %2d/80", 0);
 	printf("%*sBecken", 38, "");
-	printf("\n\n(H): %d", 0);
-	printf("%*sKinder", 30, "");
+	printf("\n\n(H): %2d", 0);
+	printf("%*sKinder", 29, "");
 	printf("%*sSchwimmer", 8, "");
 	printf("%*sAussen", 8, "");
-	printf("\n[P]: %d", 0);
-	printf("%*s%3d", 33, "", 0);	/* Kinder */
+	printf("\n[P]: %2d", autoParkplatz);
+	printf("%*s%3d", 32, "", 0);	/* Kinder */
 	printf("%*s%3d", 14, "", 0);	/* Schwimmer */
 	printf("%*s%3d", 11, "", 0);	/* Außen */
 	
@@ -168,7 +306,8 @@ void ausgabeVerarbeitung(int simMinute) {
 		printf("%d Uhr", simMinute % 60);
 	}
 	
-	printf("\nPersonen im Schwimmbad: %4d", 0);
+	printf("\nPersonen im Schwimmbad: %4d", badegaesteGesamtMenge);
+	printf("\nPersonen zu Fuss: %10d", fussgaengerGesamt);
 	printf("\n...(T)urbo");
 	printf("\n...(P)ause");
 }
